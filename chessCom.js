ee139ef8f6b6a35f380936d0d9af27b7f65a83f1
patdel0 <zefiru@protonmail.com>
2023-09-10 09:00:07 +0100

    Refactor: Prettify with `prettier`

--- chessCom.js
+++ chessCom.js
@@ -1,56 +1,57 @@
 const SELECTORS = {
-    backButton: '.game-controls-secondary-button-icon.icon-font-chess.chevron-left',
-    shareButton: '.game-controls-secondary-button-icon.icon-font-chess.share',
-    closeButton: '.icon-font-chess.x.ui_outside-close-icon',
-    fenInputField: '.share-menu-tab-pgn-section > .ui_v5-input-component',
+  backButton:
+    ".game-controls-secondary-button-icon.icon-font-chess.chevron-left",
+  shareButton: ".game-controls-secondary-button-icon.icon-font-chess.share",
+  closeButton: ".icon-font-chess.x.ui_outside-close-icon",
+  fenInputField: ".share-menu-tab-pgn-section > .ui_v5-input-component",
 };
 
 function clickElement(selector) {
-    const element = document.querySelector(selector);
-    element?.click();
+  const element = document.querySelector(selector);
+  element?.click();
 }
 
 async function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function goBackOneMove() {
-    clickElement(SELECTORS.backButton);
+  clickElement(SELECTORS.backButton);
 }
 
 function openFENmodal() {
-    clickElement(SELECTORS.shareButton);
+  clickElement(SELECTORS.shareButton);
 }
 
 function closeFENmodal() {
-    clickElement(SELECTORS.closeButton);
+  clickElement(SELECTORS.closeButton);
 }
 
 async function grabFEN() {
-    goBackOneMove();
-    openFENmodal();
+  goBackOneMove();
+  openFENmodal();
 
-    await wait(200);
+  await wait(200);
 
-    const fenInputField = document.querySelector(SELECTORS.fenInputField);
-    const fenValue = fenInputField?.value;
+  const fenInputField = document.querySelector(SELECTORS.fenInputField);
+  const fenValue = fenInputField?.value;
 
-    closeFENmodal();
+  closeFENmodal();
 
-    return fenValue;
+  return fenValue;
 }
 
-const body = document.querySelector('body');
-const ankiButton = document.createElement('button');
+const body = document.querySelector("body");
+const ankiButton = document.createElement("button");
 ankiButton.id = "ankiButton";
 ankiButton.textContent = "Analyse";
 
-ankiButton.addEventListener('click', async function () {
-    const FEN = await grabFEN();
+ankiButton.addEventListener("click", async function () {
+  const FEN = await grabFEN();
 
-    if(!FEN) return alert('Failed to retrieve FEN.');
+  if (!FEN) return alert("Failed to retrieve FEN.");
 
-    window.open(`https://lichess.org/analysis/standard/${FEN}`, '_blank')
+  window.open(`https://lichess.org/analysis/standard/${FEN}`, "_blank");
 });
 
 body.appendChild(ankiButton);
